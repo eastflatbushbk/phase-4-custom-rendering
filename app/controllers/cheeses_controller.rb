5362656910d219c263a56af8f9ef15e2c27acf0d
Robert Sainton <robertsainton@outlook.com>
2023-01-30 00:23:23 -0500

    completed

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -3,17 +3,13 @@ class CheesesController < ApplicationController
   # GET /cheeses
   def index
     cheeses = Cheese.all
-  render json: cheeses, except: [:created_at, :updated_at]
+    render json: cheeses
   end
 
   # GET /cheeses/:id
   def show
     cheese = Cheese.find_by(id: params[:id])
-  if cheese
-    render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
-  else
-    # status: :not_found will produce a 404 status code
-    render json: { error: 'Cheese not found' }, status: :not_found
+    render json: cheese
   end
 
 end
